98238ed9be9a016a9725eccc74d8bf3291d98d67
Mario Espinoza Aguayo <mario@magrocode.com>
2021-06-17 17:45:21 -0400

    first commit

---
+++ src/test/java/com/codelious/microservicios/app/gateway/microserviciosgateway/MicroserviciosGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codelious.microservicios.app.gateway.microserviciosgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviciosGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
